322f3648a1172a5004d1962a86aebd108f972af5
Erwin de Keijzer <erwin@dekeijzer.xyz>
2025-01-06 11:52:55 +0100

    Updated mise config and fish ordering

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
+    fish_add_path ~/.local/bin
+
     alias k kubectl
     alias z "zellij attach --create"
     alias tailscale /Applications/Tailscale.app/Contents/MacOS/Tailscale
@@ -20,15 +22,14 @@ if status is-interactive
     set -g fish_cursor_replace_one underscore blink
     set -g fish_cursor_visual block blink
 
+    mise activate fish | source
+
     starship init fish | source
     enable_transience
 
     bind -M insert -k nul c-space
 
     zoxide init --cmd cd fish | source
-    percli completion fish | source
     atuin init fish | source
-    dasel completion fish | source
-    mise activate fish | source
 
 end
